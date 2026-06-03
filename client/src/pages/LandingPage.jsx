cb4663c39d0b6493f99d747cec2e10bc08cdf044
Kushal S <kushals0223@gmail.com>
2026-06-03 17:45:32 +0530

    Landing page cration

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Products from '../Components/Products'
+
+const LandingPage = () => {
+    return (
+        <div className='landingpage'>
+          <Products/>
+        </div>
+    )
+}
+
+export default LandingPage
