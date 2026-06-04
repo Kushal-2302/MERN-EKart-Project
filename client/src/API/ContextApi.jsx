448f12bdf4915e35b785bb10d7f47a36ca3e68b0
Kushal S <kushals0223@gmail.com>
2026-06-04 17:32:36 +0530

    added useNavigate hook

--- client/src/API/ContextApi.jsx
+++ client/src/API/ContextApi.jsx
@@ -1,9 +1,12 @@
 import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom'
 import App from "../App";
 
 export const EkartApi = createContext();
 
 const ContextApi = () => {
+  
+  //! Get All Products
   let [ekartObj, setEkartObj] = useState({});
   let getApiData = async () => {
     try {
@@ -21,9 +24,20 @@ const ContextApi = () => {
   useEffect(() => {
     getApiData();
   }, []);
+
+
+  //! Get Perticular products
+  let [singleProduct , setSingleProduct] = useState({})
+  let navigate = useNavigate()
+  let handleViewMore = async (id) => {
+    let apiResp = await fetch(`http://localhost:5000/ekartApi/ekart/${id}`)
+    let res = await apiResp.json();
+    setSingleProduct(res)
+    navigate('/viewmore')
+  }
   return (
     <>
-      <EkartApi.Provider value={ekartObj}>
+    <EkartApi.Provider value={{ekartObj , handleViewMore , singleProduct}}>
         <App />
       </EkartApi.Provider>
     </>
