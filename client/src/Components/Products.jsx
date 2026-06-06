47d1aa19bd3de58857fdeb8b6da8470a2b340a91
Kushal S <kushals0223@gmail.com>
2026-06-06 17:05:40 +0530

    added turnary operator logic for both admin and users

--- client/src/Components/Products.jsx
+++ client/src/Components/Products.jsx
@@ -1,13 +1,21 @@
 import React, { useContext, useState } from "react";
-import { EkartApi } from "../API/ContextApi";
+import { EkartApi , AdminContext } from "../API/ContextApi";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Products = () => {
+  let navigate = useNavigate()
   const { ekartObj, handleViewMore } = useContext(EkartApi) || {};
   // console.log(ekartObj)
   // console.log(handleViewMore)
 
   let products = ekartObj.payload || [];
   // console.log(products)
+
+  let location = useLocation();
+  let path = location.pathname.startsWith("/me-as-admin");
+  // console.log(path)
+
+  let {handleDeleteOneProduct} = useContext(AdminContext)
 
   //! handle Button
   const [counts, setCounts] = useState({});
@@ -33,7 +41,10 @@ const Products = () => {
   };
   return (
     <div className="products">
-      <h2>All Products</h2>
+      <div className="header">
+        <h2>All Products</h2>
+        <button onClick={()=>navigate('/me-as-admin/addproducts')}>Add Products</button>
+      </div>
       <div className="container">
         {products.map((elem, index) => {
           let { _id, name, image } = elem;
@@ -43,19 +54,23 @@ const Products = () => {
                 <img src={image} alt="" onClick={() => handleViewMore(_id)} />
               </div>
               <div className="title">{name}</div>
-              <div className="box">
-                {(counts[_id] || 0) === 0 ? (
-                  <button className="add-btn" onClick={() => increase(_id)}>
-                    Add
-                  </button>
-                ) : (
-                  <div className="counter">
-                    <button onClick={() => decrease(_id)}>-</button>
-                    <span>{counts[_id]}</span>
-                    <button onClick={() => increase(_id)}>+</button>
-                  </div>
-                )}
-              </div>
+              {!path ? (
+                <div className="box">
+                  {(counts[_id] || 0) === 0 ? (
+                    <button className="add-btn" onClick={() => increase(_id)}>
+                      Add
+                    </button>
+                  ) : (
+                    <div className="counter">
+                      <button onClick={() => decrease(_id)}>-</button>
+                      <span>{counts[_id]}</span>
+                      <button onClick={() => increase(_id)}>+</button>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <button className="del-btn" onClick={()=>handleDeleteOneProduct(_id)}>Delete</button>
+              )}
             </div>
           );
         })}
