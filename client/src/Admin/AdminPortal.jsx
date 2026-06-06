e2e674b1db93a61e1cab147549cbbb6c6fd69d0d
Kushal S <kushals0223@gmail.com>
2026-06-06 17:01:06 +0530

    admin portal

---
+++ client/src/Admin/AdminPortal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Products from '../Components/Products'
+
+const AdminPortal = () => {
+    return (
+        <div>
+            <Products/>
+        </div>
+    )
+}
+
+export default AdminPortal
