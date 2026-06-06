0da5ecbfb4a655bec8b45edc0b09714a6a2fbc37
Kushal S <kushals0223@gmail.com>
2026-06-06 17:02:26 +0530

    background color modified

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 // import {logo} from '../assets/images/ekart-logo.png'
+import { EkartApi } from '../API/ContextApi'
+import { useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+    let {handleSearchBar} = useContext(EkartApi)
+
+    let location = useLocation()
+    let path = location.pathname
+    let pathBoolMain = path.startsWith(`/me-as-admin`)
+    let pathBoolNested = path.startsWith(`/me-as-admin/addproducts`)
     return (
         <>
             <div className="navbar">
                 <div className="logo">LOGO</div>
+                {!pathBoolNested && 
                 <div className="search">
-                    <input type="text" placeholder='Search Items'/>
-                </div>
+                    <input 
+                        type="text" 
+                        placeholder='Search Items'
+                        name='name'
+                        onChange={handleSearchBar}
+                    />
+                </div>}
+                {!pathBoolMain &&
                 <div className="links">
                     <div className="login">🙍‍♂️</div>
                     <div className="cart">🛒</div>
-                </div>
+                </div>}
             </div>
         </>
     )
